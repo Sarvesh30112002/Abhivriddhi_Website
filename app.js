ff7a269452399cfec0639dfc941a613f32de70b2
Shrikant06112002 <100031999+Shrikant06112002@users.noreply.github.com>
2023-06-01 19:22:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const hamburger_menu = document.querySelector(".hamburger-menu");
-const container = document.querySelector(".container");
+const container = document.querySelector(".containerr");
 
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
